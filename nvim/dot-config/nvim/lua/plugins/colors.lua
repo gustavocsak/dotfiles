300ac44365a280941d46e1471ba0b7ff5c17b7f2
Gustavo <gustavocsak@gmail.com>
2025-10-23 18:07:29 -0700

    update

--- nvim/dot-config/nvim/lua/plugins/colors.lua
+++ nvim/dot-config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,7 @@
--- lua/plugins/rose-pine.lua
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"nyoom-engineering/oxocarbon.nvim",
+	name = "oxocarbon",
 	config = function()
-		vim.cmd("colorscheme rose-pine")
+		vim.cmd("colorscheme oxocarbon")
 	end
 }
--- nvim/dot-config/nvim/lua/plugins/mason.lua
+++ nvim/dot-config/nvim/lua/plugins/mason.lua
@@ -1,63 +1,117 @@
 return {
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  dependencies = {
-    'nvim-lspconfig',
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      -- List the servers you want Mason to ensure are installed
-      ensure_installed = { "tsserver", "pyright", "jdtls" }, -- Add your desired servers
-      automatic_installation = true,
-    })
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require('lspconfig')
 
-    local lspconfig = require('lspconfig')
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- Capabilities (for nvim-cmp support)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local cmp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+      if cmp_ok then
+        capabilities = cmp_nvim_lsp.default_capabilities()
+      end
 
-    -- Configure tsserver
-    lspconfig.tsserver.setup {
-      capabilities = capabilities,
-      settings = {
-        typescript = {
-          tsdk = {
-            directory = "", -- Leave empty for global tsc
-          },
-        },
-        javascript = {
-          format = {
-            enable = true,
-            semicolons = 'insert',
-            singleQuote = true,
-          },
-        },
-      },
-    }
+      -- Key mappings when LSP attaches
+      local on_attach = function(_, bufnr)
+        local nmap = function(keys, func, desc)
+          if desc then
+            desc = 'LSP: ' .. desc
+          end
+          vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+        end
 
-    -- Configure pyright (example)
-    lspconfig.pyright.setup {
-      capabilities = capabilities,
-      -- Add pyright specific settings if needed
-    }
+        nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+        nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+        nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        nmap('<leader>f', function()
+          vim.lsp.buf.format { async = true }
+        end, '[F]ormat')
 
-    -- Configure jdtls (example - remember to set your JDK path)
-    lspconfig.jdtls.setup {
-      capabilities = capabilities,
-      settings = {
-        java = {
-          configuration = {
-            runtimes = {
-              {
-                name = "Java 23", -- Or your preferred Java version
-                path = "/usr/bin/java", -- Replace with your JDK path
-                default = true,
+        vim.api.nvim_create_autocmd("CursorHold", {
+          buffer = bufnr,
+          callback = function()
+            vim.diagnostic.open_float(nil, { focus = false })
+          end,
+        })
+      end
+
+      -- Setup LSP servers
+      -- clangd (C/C++)
+      lspconfig.clangd.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }
+
+      -- tsserver (TypeScript/JavaScript)
+      lspconfig.ts_ls.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }
+
+      -- pyright (Python)
+      lspconfig.pyright.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }
+
+      -- jdtls (Java)
+      lspconfig.jdtls.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          java = {
+            configuration = {
+              runtimes = {
+                {
+                  name = "Java 23",
+                  path = "/usr/bin/java", -- Change if needed
+                  default = true,
+                }
               }
             }
           }
         }
       }
-    }
 
-    -- Add configurations for other language servers you use here
-  end,
+      -- gopls (Go)
+      lspconfig.gopls.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          gopls = {
+            -- Optional: Add any gopls specific settings here.
+            -- For example, to enable staticcheck:
+            analyses = {
+              staticcheck = true,
+            },
+          },
+        },
+      }
+    end,
+  },
+
+  {
+    'williamboman/mason.nvim',
+    build = ':MasonUpdate',
+    config = function()
+      require('mason').setup()
+    end,
+  },
+
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'neovim/nvim-lspconfig',
+    },
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = { 'clangd', 'ts_ls', 'pyright', 'jdtls', 'gopls' },
+        automatic_installation = true,
+      }
+    end,
+  },
 }
+
